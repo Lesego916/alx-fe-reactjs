0593c78a13a845865dc7afd12ef466239dd56331
Lesego916 <segobuyisile@gmail.com>
2025-09-14 23:32:21 +0200

    FE

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
       <h2>Main Content</h2>
-      <p>This is the main section of the app.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
