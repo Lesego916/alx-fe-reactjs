8b8da8c9c7ae1f45c7b25c9e03065557a456ee22
Lesego916 <segobuyisile@gmail.com>
2025-09-28 21:08:46 +0200

    FE

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,6 +7,7 @@ function UserProfile() {
         src="https://via.placeholder.com/150"
         alt="User"
         className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300">
         John Doe
