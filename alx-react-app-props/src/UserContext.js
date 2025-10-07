e7585bea329f290d25114611cdeb53640b220138
Lesego916 <segobuyisile@gmail.com>
2025-10-07 19:35:08 +0200

    FE

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,13 @@
+// src/UserContext.js
 import { createContext } from "react";
 
-const UserContext = createContext();
+/**
+ * UserContext
+ * Provides a place to store and consume user-related data across the app
+ * without prop drilling.
+ *
+ * Default value is null so consumers can detect absence of a provider.
+ */
+const UserContext = createContext(null);
 
 export default UserContext;
